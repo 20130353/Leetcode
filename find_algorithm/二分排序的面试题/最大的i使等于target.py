ad025d2fbd46b79e3486a1024803dfe1647c5b27
smx <739400043@qq.com>
2018-10-26 23:49:45 -0400

    merge sort

--- find_algorithm/二分排序的面试题/最大的i使等于target.py
+++ find_algorithm/二分排序的面试题/最大的i使等于target.py
@@ -1,28 +1,27 @@
 # -*- coding: utf-8 -*-
 # Author: sunmengxin
 # time: 10/25/18
-# file: 最大的i使小于等于target.py
+# file: 最大的i使等于target.py
 # description:
-'''
-    给定数组,找一个最大的i使其小于等于target
-'''
+
 
 def find(data, target):
     if len(data) <= 0:
         return -1
     low,high = 0, len(data)-1
     while low < high:
-        mid = int((low + high)/2)
+        # 同时在这里 多 加 1, 因为如果不加1 会出现死循环的状态
+        mid = int((low + high + 1)/2)
         # mid = low + (high - low) >> 2 在python中不可以这么做...为什么?
-        if data[mid] < target:
-            low = mid + 1
+        if data[mid] > target:
+            high = mid - 1
         else:
             # data[mid] >= target
             # 这边可能会出现等于的情况
-            # 如果D[mid] == target的话 high 就会一直不变
-            # 所以high的作用的是一直守着第一个等于target的mid位置
-            # 直到low > high 所以,low 就是一个第一个等于target的位置
-            high = mid #
+            # 如果D[mid] == target的话 low 就会一直不变
+            # 所以low的作用的是一直守着第一个等于target的mid位置
+            # 在等于的时候不变
+            low = mid
     if data[low] != target:
         return -1
     else:

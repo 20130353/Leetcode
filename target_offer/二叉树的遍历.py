5b0522ddf28b8e1316fce49e990a5ee63324aa16
20130353 <739400043@qq.com>
2018-11-28 17:33:22 +0800

    update

--- target_offer/二叉树的遍历.py
+++ target_offer/二叉树的遍历.py
@@ -8,78 +8,13 @@
 import numpy as np
 
 class Node:
-    def __init__(self,data):
-        self.data = data
-        self.lchild = None
-        self.rchild = None
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
 
-class BST:
-    def __init__(self):
-        self.T = None
 
-    def search_node(self,T,parent,data):
-        if T == None:
-            return False,T,parent
-        if T.data == data:
-            return True,T,parent
-        if T.data < data:
-            return self.search_node(T.rchild,T,data)
-        else:
-            return self.search_node(T.lchild,T,data)
-
-    def insert_node(self,T,data):
-        if T == None:
-            return Node(data)
-        else:
-            if T.data < data:
-                T.rchild = self.insert_node(T.rchild,data)
-            else:
-                T.lchild = self.insert_node(T.lchild,data)
-        return T
-
-    def build_Tree(self, data):
-        self.T = Node(data[0])
-        for i in range(1,len(data)):
-             self.T = self.insert_node(self.T,data[i])
-
-    def delete(self,T,data):
-        # CN is current node with data
-        # PN is the parent node of the current node
-        flag,CN,PN = self.search_node(self.T,self.T,data)
-
-        if CN.rchild == None and CN.lchild == None:
-            if CN == PN.lchild:
-                PN.lchild = None
-            else:
-                PN.rchild = None
-
-        if CN.lchild == None and CN.rchild != None:
-            if CN == PN.lchild:
-                PN.lchild = CN.rchild
-            else:
-                PN.rchild = CN.rchild
-        if CN.rchild == None and CN.lchild != None:
-            if CN == PN.lchild:
-                PN.lchild = CN.lchild
-            else:
-                PN.rchild = CN.lchild
-        else:
-            front_node = T.lchild
-            while(front_node.rchild != None):
-                front_node  = front_node.rchild
-
-            T.data = front_node.data
-            T.lchild.rchild = front_node.lchild
-            del front_node
-        return
-
-    def delete_node(self,T,data):
-        if T.data == data:
-            self.delete(T,data)
-        elif T.data < data:
-            self.delete_node(T.rchild,data)
-        else:
-            self.delete_node(T.lchild,data)
+class Solution:
 
     def preorder_traverse(self,T):
         if T != None:
@@ -99,81 +34,95 @@ class BST:
             self.postorder_traverse(T.rchild)
             print(T.data)
 
+    # 前序和后序的区别就是放入节点的顺序不一样
+    # 中序是一直把所有的左孩子放入栈中
+    # 层次遍历和前中后序遍历的差别是层次遍历是队列,前中后序遍历是栈
+    def pre_vis(self, root):
 
-    # # 先序遍历--栈
-    # preOrder每次都将遇到的节点压入栈，当左子树遍历完毕后才从栈中弹出最后一个访问的节点，访问其右子树。
-    # 在同一层中，不可能同时有两个节点压入栈，因此栈的大小空间为O(h)，h为二叉树高度。
-    # 时间方面，每个节点都被压入栈一次，弹出栈一次，访问一次，复杂度为O(n)。
-    def previsit_stack(self, root):
-        if root == None:
-            return
+        if not root:
+            return []
+        stack = [root]
+        res = []
+        while stack:
+            top = stack.pop()
+            res.append(top.val)
+            if top.right:
+                stack.append(top.right)
+            if top.left:
+                stack.append(top.left)
+        return res
+
+    def in_vis(self, root):
+
+        if not root:
+            return []
+
+        res = []
         stack = []
-        node = root #node  相当与是指针,从根节点开始访问
+        node = root
         while node or stack:
             while node:
-                # 从根节点开始，一直找它的左子树
-                print(node.data)
                 stack.append(node)
-                node = node.lchild
-            # while结束表示当前节点node为空，即前一个节点没有左子树了
-            node = stack.pop() #从栈中弹出最后一个访问的节点,根节点
-            # 开始查看它的右子树
-            node = node.rchild
+                node = node.left
 
-    # 先序和中序只是改变一下打印的位置
-    def invisit_stack(self, root):
-        if root == None:
-            return
-        stack = []
-        node = root  # node  相当与是指针,从根节点开始访问
-        while node or stack:
-            while node:
-                # 从根节点开始，一直找它的左子树
-                stack.append(node)
-                node = node.lchild
-            # while结束表示当前节点node为空，即前一个节点没有左子树了
-            node = stack.pop()  # 从栈中弹出最后一个访问的节点,根节点
-            print(node.data)
-            # 开始查看它的右子树
-            node = node.rchild
-
-    # 从直觉上来说，后序遍历对比中序遍历难度要增大很多。因为中序遍历节点序列有一点的连续性，
-    # 而后续遍历则感觉有一定的跳跃性。先左，再右，最后才中间节点；
-    # 访问左子树后，需要跳转到右子树，右子树访问完毕了再回溯至根节点并访问
-    def postorder_stack(self,T):
-        if T == None:
-            return
-        stack = [T]
-        record = []
-        while (len(stack) >0):
             node = stack.pop()
-            record.append(node)
-            if node.rchild:
-                stack.append(node.rchild)
-            if node.lchild:
-                stack.append(node.lchild)
+            res.append(node.val)
+            node = node.right
+        return res
 
-        for each in range(len(record)-1,0,-1):
-            print(each.data)
+    def post_vis(self,root):
 
+        if not root:
+            return []
 
-    def level_stack(self,T):
-        if T == None:
-            return
-        queue = [T]
-        while(len(queue)>0):
-            node = queue.pop()
-            print(node.data)
-            if node.lchild:
-                queue.append(node.lchild)
-            if node.rchild:
-                queue.append(node.rchild)
+        stack = [root]
+        res = []
+        while stack:
+            top = stack.pop()
+            res.insert(0,top.val)
+            if top.left:
+                stack.append(top.left)
+            if top.right:
+                stack.append(top.right)
+        return res
 
+    # 反思:
+    # 1. queue进行弹出操作的时候注意queue是否为空. 栈也是同理!
+    # 2. 没有返回函数值
+    def level_vis(self, root):
+
+        if not root:
+            return []
+
+        res = []
+        queue = [root]
+        while queue:
+            top = queue.pop(0)
+            res.append(top.val)
+            if top.left:
+                queue.append(top.left)
+            if top.right:
+                queue.append(top.right)
+        return res
 
 
 if __name__ == '__main__':
-    # data = [62,88,58,47,35,73,51,99,37,93]
-    data = [62,58,88,47,73,99,35,51,93,29,37,49,56,36,48,50]
-    Tree = BST()
-    Tree.build_Tree(data)
-    Tree.inorder_stack(Tree.T)
+
+    root = Node(0)
+    Node1 = Node(1)
+    Node2 = Node(2)
+    Node3 = Node(3)
+    Node4 = Node(4)
+    Node5 = Node(5)
+    Node6 = Node(6)
+    Node7 = Node(7)
+
+    root.left = Node2
+    root.right = Node3
+    Node2.left = Node4
+    Node2.right = Node5
+    Node3.left = Node6
+    Node3.right = Node7
+
+    so = Solution()
+    print(so.post_vis(root))
--- target_offer/连续子数组的最大和.py
+++ target_offer/连续子数组的最大和.py
@@ -8,22 +8,22 @@
     给定一个数组,输出数组中连续的子数组的最大和
 '''
 
+
 class Solution:
     def FindGreatestSumOfSubArray(self, array):
         # write code here
         res = []
-        for inx,each in enumerate(array):
+        for inx, each in enumerate(array):
             if not res:
                 res.append(each)
             else:
-                res.append(max(each,each+res[-1]))
+                res.append(max(each, each + res[-1]))
         # index = res.index(max(res))
         return max(res)
 
 
-
 if __name__ == '__main__':
-    data = [1,-1,3,10,-4,7,2,-5]
-    so =Solution()
+    data = [1, -1, 3, 10, -4, 7, 2, -5]
+    so = Solution()
     res = so.FindGreatestSumOfSubArray(data)
     print(res)

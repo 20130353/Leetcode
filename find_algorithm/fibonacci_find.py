fbcb6a34d5a41aa455bc0ae112f3cf9c667e7618
smx <739400043@qq.com>
2019-07-01 00:27:03 -0400

    Update fibonacci_find.py
    mix bug

--- find_algorithm/fibonacci_find.py
+++ find_algorithm/fibonacci_find.py
@@ -1,19 +1,11 @@
-# -*- coding: utf-8 -*-
-# Author: sunmengxin
-# time: 10/8/18
-# file: fibonacci_find.py
-# description:
-
-import numpy as np
-
 def create_fa():
     res = [1, 1]
-    for i in range(1, 20):
-        res = res[i - 1] + res[i - 2]
+    for i in range(2, 20):
+        res.append(res[i - 1] + res[i - 2])
     return res
 
 
-def fa_find(arr, n, val):
+def fibonacc_find(arr, n, val):
     F = create_fa()
 
     low, high = 0, n - 1
@@ -25,12 +17,12 @@ def fa_find(arr, n, val):
 
     while low < high:
         mid = low + F[k - 1] - 1
-        #这里有两种情况， 1：可能是补全的值 2：可能是正常值， 用位置n-1做个判断
+        # 这里有两种情况， 1：可能是补全的值 2：可能是正常值， 用位置n-1做个判断
         if arr[mid] == val:
             if mid < n:
                 return mid
             else:
-                return n-1
+                return n - 1
         elif arr[mid] < val:
             low = mid
             k -= 2
@@ -40,8 +32,10 @@ def fa_find(arr, n, val):
     return -1
 
 
+import numpy as np
+
 if __name__ == '__main__':
     data = np.random.rand(10)
     data = sorted(data)
-    inx = fibonacc_find(data,data[7])
+    inx = fibonacc_find(data, len(data), data[7])
     print(inx)

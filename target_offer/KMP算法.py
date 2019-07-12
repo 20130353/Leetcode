75a0d1ba71c1727fc3d050a980721b6274691be2
smx <739400043@qq.com>
2019-07-12 01:13:24 -0400

    Update KMP算法.py
    增加对应的习题

--- target_offer/KMP算法.py
+++ target_offer/KMP算法.py
@@ -10,7 +10,8 @@ def get_next(str):
     i = 2
     while i < len(str):
         if str[i - 1] == str[cn]:
-            next[i] = cn + 1
+            cn += 1
+            next.append(cn)
             i += 1
         elif cn > 0:
             cn = next[cn]
@@ -38,8 +39,31 @@ def KMP(str1, str2):
     else:
         return False, -1
 
+    
+# 对应的练习题, 给定一个字符串,求最小的无重复子串, 要求子串能通过n次拼接得到原来的字符串
+# 解题关键是: 给字符串增加一个字符,求它的next数组, 最后的next数组值就是子串的长度
+def get_min_substr(str):
+    next = get_next(str + '#')
+    return next[-1], str[:next[-1]]
+ 
+    
+# 对应的练习题: 给一个字符串, 求2个字符串拼接后去掉相连的字符的长度
+def get_min_str(str):
+    next = get_next(str + '#')
+    return 2 * len(str) - next[-1]
+
 
 if __name__ == '__main__':
     str1, str2 = '45102345', '123'
     ans = KMP(str1, str2)
     print(ans)
+    
+    
+    str = 'abcabc'
+    ans = get_min_substr(str)
+    print(ans)
+    
+    str = 'abca'
+    ans = get_min_str(str)
+    print(ans)
+

a48e6ce208e06b98ec22f8042b4e16281d5c7835
smx <739400043@qq.com>
2019-07-12 00:07:35 -0400

    Update KMP算法.py
    更简单的方式理解KMP算法

--- target_offer/KMP算法.py
+++ target_offer/KMP算法.py
@@ -1,54 +1,45 @@
-# -*- coding: utf-8 -*-
-# author: sunmengxin
-# time: 18-11-26
-# file: KMP算法
-# description:
+def get_next(str):
+    if len(str) == 1:
+        return [-1]
 
-class KMP:
+    if len(str) == 2:
+        return [-1, 0]
 
-    def prefix_table(self, pattern, n):
+    next = [-1, 0]
+    cn = 0
+    i = 2
+    while i < len(str):
+        if str[i - 1] == str[cn]:
+            next[i] = cn + 1
+            i += 1
+        elif cn > 0:
+            cn = next[cn]
+        else:
+            next.append(0)
+            i += 1
+    return next
 
-        prefix = [0 for _ in range(n)]
-        len = 0  # 之前匹配的字符的长度
-        i = 1
-        while i < n:
-            if pattern[i] == pattern[len]:  # 之前已经匹配的长度的后一个字符和现在看的这个字符是是否一样的(这个字符本身就是当前的后缀的最后一个字符)
-                len += 1
-                prefix[i] = len
+
+def KMP(str1, str2):
+    i, j = 0, 0
+    next = get_next(str2)
+    while i < len(str1) and j < len(str2):
+        if str1[i] == str2[j]:
+            i += 1
+            j += 1
+        else:
+            if next[j] == -1:
                 i += 1
             else:
-                if len > 0:
-                    len = prefix[len - 1]  # 斜着对
-                else:
-                    prefix[i] = len  # 对到第一个字符了,之前没有字符可以继续对了
-                    i += 1
+                j = next[j]
 
-        #     将整个prefixtable向后一位,首位是-1
-        prefix.insert(0, -1)
-        return prefix
-
-    def kmp(self, text, pattern):
-
-        n, m = len(pattern), len(text)
-        prefix = self.prefix_table(pattern, n)
-        i, j = 0, 0
-        while i < m:  # 从头开始找,一直找到结尾
-            if j == n:
-                print('found at', i - j)
-                j = prefix[j]
-            if text[i] == pattern[j]:
-                i += 1
-                j += 1
-            else:
-                j = prefix[j]
-                if j == -1:  # 找到最末尾的地方,依然无法匹配只能都向后移动,重新开始匹配
-                    i += 1
-                    j += 1
-        return
+    if j == len(str2):
+        return True, i - j
+    else:
+        return False, -1
 
 
 if __name__ == '__main__':
-    pattern = 'ABABA'
-    text = 'AAAABABABABACC'
-    KMP = KMP()
-    print(KMP.kmp(text, pattern))
+    str1, str2 = '45102345', '123'
+    ans = KMP(str1, str2)
+    print(ans)

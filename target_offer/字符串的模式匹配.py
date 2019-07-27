e1195a6cd5dd5612a4dc20407db05387a7e090fa
smx <739400043@qq.com>
2019-07-27 17:44:10 +0800

    Update 字符串的模式匹配.py

--- target_offer/字符串的模式匹配.py
+++ target_offer/字符串的模式匹配.py
@@ -5,6 +5,69 @@
 # description:
 
 
+最新写的代码：
+# 虽然写的有些啰嗦，但是也是全对的，可以重点看一下case！
+# -*- coding:utf-8 -*-
+class Solution:
+    def dfs(self, i, j, s, pat, flag):
+        if flag[0]:
+            return
+        if i >= len(s) and j >= len(pat):
+            flag[0] = True
+            return
+        if j >= len(pat) and i < len(s):
+            return
+
+        # 这两段可以合并！
+        if pat[j] == '.' and j + 1 < len(pat) and pat[j + 1] == '*':
+            self.dfs(i, j + 2, s, pat, flag)
+            if flag[0]:
+                return
+            # 这一段可以写成不同*
+            for k in range(len(s) - i + 1):
+                self.dfs(i + k, j + 2, s, pat, flag)
+                if flag[0]:
+                    return
+        if j + 1 < len(pat) and pat[j + 1] == '*':
+            if i < len(s):
+                self.dfs(i, j + 2, s, pat, flag)
+                if s[i] == pat[j]:
+                    while s[i] == pat[j]:
+                        self.dfs(i + 1, j + 2, s, pat, flag)
+                        if flag[0]:
+                            return
+                        i = i + 1
+            else:
+                self.dfs(i, j + 2, s, pat, flag)
+
+        if (i < len(s) and pat[j] == '.') or (i < len(s) and s[i] == pat[j]):
+            self.dfs(i + 1, j + 1, s, pat, flag)
+
+    def match(self, s, pattern):
+        flag = [False]
+        self.dfs(0, 0, s, pattern, flag)
+        return flag[0]
+
+
+if __name__ == '__main__':
+    string = ['', '.*']
+    string = ['', 'a']
+    string = ['', 'a*']
+    string = ['', '.']
+    string = ['aaaaaa', 'a*a']
+    string = ['aaaaaa', '*a']
+    string = ['aaaaaa', 'aaaaaaaaaa']
+    string = ['aaaaaa', 'aa']
+    string = ['aaaaaa', 'aaaaaa']
+    string = ['ababaa', 'a.a*baa']
+    string = ["bcbbabab", ".*a*a"]
+    string = ['a', '.*']
+
+    flag = [False]
+    ans = Solution().match(string[0], string[1])
+    print(ans)
+
+
 # 反思:
 # 1. 值得肯定的是我刚开始做出来了一部分,但是缺点是我对于*
 # 能考虑的状况少了一部分,这个以后要注意!
@@ -79,3 +142,5 @@ if __name__ == '__main__':
     print(so.match('aaa', 'aa.a'))  # False
     print(so.match('aaa', 'ab*a'))  # False
     print(so.match(',', '.*'))  # True
+    
+   
